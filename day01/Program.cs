fd0671919ebc81e373db275314a96c198c2b43a8
Lars Nyström <nystrom.lasse@gmail.com>
2022-12-01 21:09:06 +0100

    Solution for day 01

--- day01/Program.cs
+++ day01/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var lines = File.ReadAllLines("input.txt")
+    .ToArray<string>();
+
+var elves = new SortedList<int, int>();
+
+var sum = 0;
+var elf = 0;
+foreach (var line in lines)
+{
+    if (string.IsNullOrWhiteSpace(line))
+    {
+        if (!elves.ContainsKey(sum))
+            elves.Add(sum, elf++);
+        sum = 0;
+    }
+    else
+    {
+        sum += int.Parse(line);
+    }
+}
+
+// Part 1
+System.Console.WriteLine(elves.Last().Key);
+
+// Part 2
+System.Console.WriteLine(elves.TakeLast(3).Sum(e => e.Key));
