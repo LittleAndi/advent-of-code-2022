763b19e2e8fd12f084739397016c6c0664993534
Lars Nyström <nystrom.lasse@gmail.com>
2022-12-01 21:12:29 +0100

    Day01 (#1)
    * Add day01
    
    * Solution for day 01

---
+++ day01/Program.cs
@@ -0,0 +1,26 @@
+﻿var lines = File.ReadAllLines("input.txt")
+    .ToArray<string>();
+
+var elves = new SortedList<int, int>();
+
+var sum = 0;
+var elf = 0;
+foreach (var line in lines)
+{
+    if (string.IsNullOrWhiteSpace(line))
+    {
+        if (!elves.ContainsKey(sum))
+            elves.Add(sum, elf++);
+        sum = 0;
+    }
+    else
+    {
+        sum += int.Parse(line);
+    }
+}
+
+// Part 1
+System.Console.WriteLine(elves.Last().Key);
+
+// Part 2
+System.Console.WriteLine(elves.TakeLast(3).Sum(e => e.Key));
