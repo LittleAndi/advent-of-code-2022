fc7980908a2b3a7bd3a8ca5da8ac423c8f98d791
Lars Nyström <nystrom.lasse@gmail.com>
2022-12-04 09:09:15 +0100

    Solution for day 04

--- day04/Program.cs
+++ day04/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var sectionAssignments = File.ReadAllLines("input.txt")
+    .Where(l => !string.IsNullOrWhiteSpace(l))
+    .Select(l => new SectionAssignmentPair(l))
+    .ToList();
+
+System.Console.WriteLine(sectionAssignments.Where(sa => sa.OneAssignmentFullyContainsTheOther).Count());
+
+System.Console.WriteLine(sectionAssignments.Where(sa => sa.AssignmentsOverlap).Count());
