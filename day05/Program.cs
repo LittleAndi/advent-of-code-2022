c594ebfa0093d071d7ba11576393644d8a4ba2ea
Lars Nyström <nystrom.lasse@gmail.com>
2022-12-04 09:50:22 +0100

    Update to .NET 7

--- day05/Program.cs
+++ day05/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var somethingSomething = File.ReadAllLines("input.txt")
+    .Where(l => !string.IsNullOrWhiteSpace(l))
+//    .Select(l => new SomethingSomething(l))
+    .ToList();
