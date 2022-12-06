4dce55ed2c966e2895be6c4270ce1780f0b8164c
Lars Nyström <nystrom.lasse@gmail.com>
2022-12-06 07:40:00 +0100

    Solution for day 06

--- day06/Program.cs
+++ day06/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var signal = File.ReadAllLines("input.txt")
+    .Where(l => !string.IsNullOrWhiteSpace(l))
+    .First();
+
+var markerDetector = new StartOfPacketMarkerDetector(signal);
+System.Console.WriteLine(markerDetector.GetMarkerEnd(4));
+System.Console.WriteLine(markerDetector.GetMarkerEnd(14));
--- tests/day06tests/UnitTest1.cs
+++ tests/day06tests/UnitTest1.cs
@@ -1,10 +1,18 @@
+using Shouldly;
+
 namespace day06tests;
 
 public class UnitTest1
 {
-    [Fact]
-    public void Test1()
+    [Theory]
+    [InlineData("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 4, 7)]
+    [InlineData("bvwbjplbgvbhsrlpgdmjqwftvncz", 4, 5)]
+    [InlineData("nppdvjthqldpwncqszvftbrmjlhg", 4, 6)]
+    [InlineData("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 4, 10)]
+    [InlineData("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 4, 11)]
+    public void ShouldGetMarkerEnd(string signal, int markerSize, int expectedMarkerEnd)
     {
-
+        var detector = new StartOfPacketMarkerDetector(signal);
+        detector.GetMarkerEnd(markerSize).ShouldBe(expectedMarkerEnd);
     }
 }
