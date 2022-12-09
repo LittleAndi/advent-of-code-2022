a71d0779ac31115e306532f9bf922773caad3178
Lars Nyström <nystrom.lasse@gmail.com>
2022-12-09 07:52:31 +0100

    Solution for part 1 of day 09

--- day09/Program.cs
+++ day09/Program.cs
@@ -1,2 +1,140 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Diagnostics.CodeAnalysis;
+
+var movements = File.ReadAllLines("input.txt")
+    .Where(l => !string.IsNullOrWhiteSpace(l))
+    .ToList();
+
+System.Console.WriteLine(movements.Count);
+
+var rope = new Rope();
+
+var tailPositions = new List<Point>();
+foreach (var movement in movements)
+{
+    tailPositions.AddRange(rope.MoveHead(movement));
+}
+
+System.Console.WriteLine($"Unique tailpositions: {tailPositions.ToHashSet().Count}");
+
+public class Rope
+{
+    Point Head;
+    public Point Tail;
+    public Rope()
+    {
+        Head = new Point() { X = 0, Y = 0 };
+        Tail = new Point() { X = 0, Y = 0 };
+    }
+
+    public IEnumerable<Point> MoveHead(string instruction)
+    {
+        var direction = instruction[0];
+        var distance = int.Parse(instruction[2..]);
+
+        var tailPositions = new HashSet<Point>();
+        switch (direction)
+        {
+            case 'U':
+                for (int i = 0; i < distance; i++)
+                {
+                    Head = Head.Up;
+                    if (!Tail.IsOnOrNextTo(Head))
+                    {
+                        (var dX, var dY) = Head.Distance(Tail);
+                        if (Math.Sign(dY) == -1) Tail = Tail.Up;
+                        if (Math.Sign(dX) == -1) Tail = Tail.Left;
+                        if (Math.Sign(dY) == 1) Tail = Tail.Down;
+                        if (Math.Sign(dX) == 1) Tail = Tail.Right;
+                    }
+                    tailPositions.Add(Tail);
+                }
+                break;
+            case 'L':
+                for (int i = 0; i < distance; i++)
+                {
+                    Head = Head.Left;
+                    if (!Tail.IsOnOrNextTo(Head))
+                    {
+                        (var dX, var dY) = Head.Distance(Tail);
+                        if (Math.Sign(dY) == -1) Tail = Tail.Up;
+                        if (Math.Sign(dX) == -1) Tail = Tail.Left;
+                        if (Math.Sign(dY) == 1) Tail = Tail.Down;
+                        if (Math.Sign(dX) == 1) Tail = Tail.Right;
+                    }
+                    tailPositions.Add(Tail);
+                }
+                break;
+            case 'D':
+                for (int i = 0; i < distance; i++)
+                {
+                    Head = Head.Down;
+                    if (!Tail.IsOnOrNextTo(Head))
+                    {
+                        (var dX, var dY) = Head.Distance(Tail);
+                        if (Math.Sign(dY) == -1) Tail = Tail.Up;
+                        if (Math.Sign(dX) == -1) Tail = Tail.Left;
+                        if (Math.Sign(dY) == 1) Tail = Tail.Down;
+                        if (Math.Sign(dX) == 1) Tail = Tail.Right;
+                    }
+                    tailPositions.Add(Tail);
+                }
+                break;
+            case 'R':
+                for (int i = 0; i < distance; i++)
+                {
+                    Head = Head.Right;
+                    if (!Tail.IsOnOrNextTo(Head))
+                    {
+                        (var dX, var dY) = Head.Distance(Tail);
+                        if (Math.Sign(dY) == -1) Tail = Tail.Up;
+                        if (Math.Sign(dX) == -1) Tail = Tail.Left;
+                        if (Math.Sign(dY) == 1) Tail = Tail.Down;
+                        if (Math.Sign(dX) == 1) Tail = Tail.Right;
+                    }
+                    tailPositions.Add(Tail);
+                }
+                break;
+        }
+        return tailPositions;
+    }
+}
+public struct Point
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+    public int Risk { get; set; }
+    public Point Up => new Point { X = X, Y = Y - 1 };
+    public Point UpLeft => new Point { X = X - 1, Y = Y - 1 };
+    public Point Left => new Point { X = X - 1, Y = Y };
+    public Point DownLeft => new Point { X = X - 1, Y = Y + 1 };
+    public Point Down => new Point { X = X, Y = Y + 1 };
+    public Point DownRight => new Point { X = X + 1, Y = Y + 1 };
+    public Point Right => new Point { X = X + 1, Y = Y };
+    public Point UpRight => new Point { X = X + 1, Y = Y - 1 };
+
+    public bool IsOnOrNextTo(Point other)
+    {
+        return
+            other.Equals(this) ||
+            other.Equals(Up) ||
+            other.Equals(UpLeft) ||
+            other.Equals(Left) ||
+            other.Equals(DownLeft) ||
+            other.Equals(Down) ||
+            other.Equals(DownRight) ||
+            other.Equals(Right) ||
+            other.Equals(UpRight);
+    }
+
+    public (int dX, int dY) Distance(Point other)
+    {
+        return (X - other.X, Y - other.Y);
+    }
+
+    public override bool Equals([NotNullWhen(true)] object obj) => (((Point)obj).X == this.X && ((Point)obj).Y == this.Y);
+
+    public override int GetHashCode()
+    {
+        return (X, Y).GetHashCode();
+    }
+}
--- tests/day09tests/UnitTest1.cs
+++ tests/day09tests/UnitTest1.cs
@@ -1,3 +1,5 @@
+using Shouldly;
+
 namespace day09tests;
 
 public class UnitTest1
@@ -5,6 +7,33 @@ public class UnitTest1
     [Fact]
     public void Test1()
     {
+        var rope = new Rope();
+        rope.MoveHead("R 4");
+        rope.Tail.ShouldBe(new Point { X = 3, Y = 0 });
+        rope.MoveHead("U 4");
+        rope.Tail.ShouldBe(new Point { X = 4, Y = -3 });
+        rope.MoveHead("L 3");
+        rope.MoveHead("D 1");
+        rope.MoveHead("R 4");
+        rope.MoveHead("D 1");
+        rope.MoveHead("L 5");
+        rope.MoveHead("R 2");
+        rope.Tail.ShouldBe(new Point { X = 1, Y = -2 });
+    }
 
+    [Fact]
+    public void Test2()
+    {
+        var tailPositions = new List<Point>();
+        var rope = new Rope();
+        tailPositions.AddRange(rope.MoveHead("R 4"));
+        tailPositions.AddRange(rope.MoveHead("U 4"));
+        tailPositions.AddRange(rope.MoveHead("L 3"));
+        tailPositions.AddRange(rope.MoveHead("D 1"));
+        tailPositions.AddRange(rope.MoveHead("R 4"));
+        tailPositions.AddRange(rope.MoveHead("D 1"));
+        tailPositions.AddRange(rope.MoveHead("L 5"));
+        tailPositions.AddRange(rope.MoveHead("R 2"));
+        tailPositions.ToHashSet().Count.ShouldBe(13);
     }
 }
