db5b96745d5733c09a16c3e9fb412d2188ffebcb
Lars Nyström <nystrom.lasse@gmail.com>
2022-12-13 00:41:25 +0100

    WIP

--- day12/Program.cs
+++ day12/Program.cs
@@ -36,38 +36,98 @@ public class HeightMap
 
     public int Climb()
     {
-        var path = new List<Point>();
-        Find(start, path);
+        (var success, var path) = Find(start, new List<Point>());
+        if (!success)
+        {
+            System.Console.WriteLine("FAILURE!");
+        }
         return path.Count;
     }
 
-    public int Find(Point p, List<Point> path)
+    public (bool success, List<Point>) Find(Point p, List<Point> path)
     {
+        System.Console.WriteLine($"{p.ToString()}: {heightMap[p.X, p.Y]}");
+
         // Add yourself
         path.Add(p);
 
         // Return if end
-        if (heightMap[p.X, p.Y].Equals('E')) return path.Count;
+        if (heightMap[p.X, p.Y].Equals('E')) return (true, new List<Point>());
 
-        if (heightMap[p.X, p.Y].Equals('z') && heightMap[p.Up.X, p.Up.Y].Equals('E')) return 1;
-        if (heightMap[p.X, p.Y].Equals('z') && heightMap[p.Left.X, p.Left.Y].Equals('E')) return 1;
-        if (heightMap[p.X, p.Y].Equals('z') && heightMap[p.Down.X, p.Down.Y].Equals('E')) return 1;
-        if (heightMap[p.X, p.Y].Equals('z') && heightMap[p.Right.X, p.Right.Y].Equals('E')) return 1;
+        // Return if next to end
+        if (heightMap[p.X, p.Y].Equals('z') && heightMap[p.Up.X, p.Up.Y].Equals('E')) return (true, new List<Point>());
+        if (heightMap[p.X, p.Y].Equals('z') && heightMap[p.Left.X, p.Left.Y].Equals('E')) return (true, new List<Point>());
+        if (heightMap[p.X, p.Y].Equals('z') && heightMap[p.Down.X, p.Down.Y].Equals('E')) return (true, new List<Point>());
+        if (heightMap[p.X, p.Y].Equals('z') && heightMap[p.Right.X, p.Right.Y].Equals('E')) return (true, new List<Point>());
 
         // Look around (we should have not been there before)
         // Find next if higher or same
-        var directionLengths = new List<int>();
-        if (!path.Contains(p.Up) && !IsOutOfBounds(p.Up) && heightMap[p.Up.X, p.Up.Y] >= heightMap[p.X, p.Y]) directionLengths.Add(Find(p.Up, new List<Point>(path)));
-        if (!path.Contains(p.Left) && !IsOutOfBounds(p.Left) && heightMap[p.Left.X, p.Left.Y] >= heightMap[p.X, p.Y]) directionLengths.Add(Find(p.Left, new List<Point>(path)));
-        if (!path.Contains(p.Down) && !IsOutOfBounds(p.Down) && heightMap[p.Down.X, p.Down.Y] >= heightMap[p.X, p.Y]) directionLengths.Add(Find(p.Down, new List<Point>(path)));
-        if (!path.Contains(p.Right) && !IsOutOfBounds(p.Right) && heightMap[p.Right.X, p.Right.Y] >= heightMap[p.X, p.Y]) directionLengths.Add(Find(p.Right, new List<Point>(path)));
+        var directionLengths = new List<List<Point>>();
+        if (
+            !path.Contains(p.Up) &&
+            !IsOutOfBounds(p.Up) &&
+            (
+                heightMap[p.Up.X, p.Up.Y] == 'a' ||
+                heightMap[p.Up.X, p.Up.Y] == heightMap[p.X, p.Y] ||
+                heightMap[p.Up.X, p.Up.Y] == heightMap[p.X, p.Y] + 1
+            )
+        )
+        {
+            (var success, var childPath) = Find(p.Up, new List<Point>(path));
+            if (success) directionLengths.Add(childPath);
+        }
+
+        if (
+            !path.Contains(p.Left) &&
+            !IsOutOfBounds(p.Left) &&
+            (
+                heightMap[p.Left.X, p.Left.Y] == 'a' ||
+                heightMap[p.Left.X, p.Left.Y] == heightMap[p.X, p.Y] ||
+                heightMap[p.Left.X, p.Left.Y] == heightMap[p.X, p.Y] + 1
+            )
+        )
+        {
+            (var success, var childPath) = Find(p.Left, new List<Point>(path));
+            if (success) directionLengths.Add(childPath);
+        }
+
+
+        if (
+            !path.Contains(p.Down) &&
+            !IsOutOfBounds(p.Down) &&
+            (
+                heightMap[p.Down.X, p.Down.Y] == 'a' ||
+                heightMap[p.Down.X, p.Down.Y] == heightMap[p.X, p.Y] ||
+                heightMap[p.Down.X, p.Down.Y] == heightMap[p.X, p.Y] + 1
+            )
+        )
+        {
+            (var success, var childPath) = Find(p.Down, new List<Point>(path));
+            if (success) directionLengths.Add(childPath);
+        }
+
+        if (
+            !path.Contains(p.Right) &&
+            !IsOutOfBounds(p.Right) &&
+            (
+                heightMap[p.Right.X, p.Right.Y] == 'a' ||
+                heightMap[p.Right.X, p.Right.Y] == heightMap[p.X, p.Y] ||
+                heightMap[p.Right.X, p.Right.Y] == heightMap[p.X, p.Y] + 1
+            )
+        )
+        {
+            (var success, var childPath) = Find(p.Right, new List<Point>(path));
+            if (success) directionLengths.Add(childPath);
+        }
 
         if (directionLengths.Count == 0)
         {
-            System.Console.WriteLine("ERROR");
+            return (false, new List<Point>());
         }
 
-        return directionLengths.OrderBy(l => l).First();
+        // return the shortest
+        var shortest = directionLengths.OrderBy(l => l.Count).First();
+        return (true, shortest);
     }
 
     private bool IsOutOfBounds(Point p)
