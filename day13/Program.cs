69ae132492ad6e7fd6eabd2e2ba2129831492454
Lars Nyström <nystrom.lasse@gmail.com>
2022-12-19 07:52:00 +0100

    WIP

--- day13/Program.cs
+++ day13/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var distressSignals = File.ReadAllLines("input.txt")
+    .ToList();
+
+System.Console.WriteLine(distressSignals[0]);
+System.Console.WriteLine(distressSignals[1]);
+
+
+
+public class SignalDecoder
+{
+    private readonly string l;
+    private readonly string r;
+
+    public SignalDecoder(string left, string right)
+    {
+        this.l = left;
+        this.r = right;
+    }
+
+    public bool InTheRightOrder
+    {
+        get
+        {
+            Stack<Thing> lStack = new Stack<Thing>();
+            Stack<Thing> rStack = new Stack<Thing>();
+
+            int lPos = 0;
+            int rPos = 0;
+
+            var t = l.Split(new char[] { '[', ']', ',' });
+
+            return l == r;
+        }
+    }
+
+    public class Thing
+    {
+        private readonly string thing;
+
+        public Thing(string thing)
+        {
+            this.thing = thing;
+        }
+    }
+}
--- tests/day13tests/UnitTest1.cs
+++ tests/day13tests/UnitTest1.cs
@@ -1,10 +1,16 @@
+using Shouldly;
+
 namespace day13tests;
 
 public class UnitTest1
 {
-    [Fact]
-    public void Test1()
+    [Theory]
+    // [InlineData("[1,1,3,1,1]", "[1,1,5,1,1]", false)]
+    [InlineData("[1,[2,[3,[4,[5,6,7]]]],8,9]", "[1,[2,[3,[4,[5,6,0]]]],8,9]", false)]
+    // [InlineData("[[1],[2,3,4]]", "[[1],4]", false)]
+    public void Test1(string left, string right, bool expectedInTheRightOrder)
     {
-
+        var signalDecoder = new SignalDecoder(left, right);
+        signalDecoder.InTheRightOrder.ShouldBe(expectedInTheRightOrder);
     }
 }
